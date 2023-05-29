1a4ba148bab813f6381cfaec85ae74c40ccd756e
GLH <56247991+gonglihai@users.noreply.github.com>
2023-05-29 11:09:13 +0800

    change workspase

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-java Wol
+function changeWorkspace(){
+  cd $(dirname $0)
+  echo "change workspase: `pwd`"
+}
+
+changeWorkspace
+/usr/bin/java Wol
+
+# crontab -e 每周1至周5 上午9:00, 网络唤醒办公电脑
+# 0 9 * * 1,2,3,4,5 /workspase/wol/start.sh
